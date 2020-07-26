2693fb24510ab72d2f25d16f05c9bd044e8bad85
nikhlagrwl <niksagarwal13@gmail.com>
2020-07-26 18:44:10 +0530

    edit

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,5 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', views.index)
+    path('index/', views.user_get_post),
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
+from user.models import User
+from user.serializer import UserSerializer
 # Create your views here.
 
-def index(request):
-	return HttpResponse("Hello World django")
-	# return render(request, "index.html")
+@csrf_exempt
+def user_get_post(request):
+	if request.method == 'GET':
+		data = User.objects.all()
+		serializer = UserSerializer(data, many = True)
+		return JsonResponse(serializer.data, safe = False)
+
+	elif request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = UserSerializer(data)
+		if(serializer.is_valid()):
+			serializer.save()
+			return JsonResponse(serializer.data, status = 201)
+		return HttpResponse(serializer.error, status = 400)
