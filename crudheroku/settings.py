0de6c1ac0441ce6559b8b3e26505ce79e6c5c10d
nikhlagrwl <niksagarwal13@gmail.com>
2020-07-26 15:37:37 +0530

    first commit

--- crudheroku/settings.py
+++ crudheroku/settings.py
@@ -50,6 +50,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
 ROOT_URLCONF = 'crudheroku.urls'
 
 TEMPLATES = [
@@ -117,6 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
